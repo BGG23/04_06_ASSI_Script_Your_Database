020dd55f9ebf16d09ca41ecc198a314cf1938de6
Belén Gamero García <91567318+BGG23@users.noreply.github.com>
2023-01-27 22:15:09 +0100

    Update and rename DropData.sql to Delete.sql

---
+++ Delete.sql
@@ -0,0 +1,7 @@
+DELETE FROM bicicleta;
+
+DELETE FROM extra;
+
+DELETE FROM bici_extra;
+
+DELETE FROM reserva;
--- DropData.sql
+++
@@ -1,7 +0,0 @@
-DELETE from bicicleta;
-
-DELETE from extra;
-
-DELETE from bici_extra;
-
-DELETE FROM reserva;
