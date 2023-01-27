873ec010771237eac9b15af37504c303b0c8f89e
Belén Gamero García <91567318+BGG23@users.noreply.github.com>
2023-01-27 22:05:44 +0100

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,11 +1,9 @@
-select * from bicicleta where Marca = "Spezialited";
+SELECT * FROM bicicleta WHERE Marca = "Spezialited";
 
-select * from reserva where importe <= 50;
+SELECT * FROM reserva WHERE importe <= 50;
 
-select * from bicicleta where extras_bicicleta = 1;
+SELECT * FROM bicicleta WHERE extras_bicicleta = 1;
 
-SELECT *
-FROM bicicleta b INNER JOIN bici_extra be 
-ON b.Id_bicicleta = be.Id_bicicleta 
-INNER JOIN extra e 
-ON e.Id_extra = be.Id_extra
+SELECT * FROM bicicleta b INNER 
+  JOIN bici_extra be ON b.Id_bicicleta = be.Id_bicicleta 
+      INNER JOIN extra e ON e.Id_extra = be.Id_extra
